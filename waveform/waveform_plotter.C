5915d924cf929ee004246e5c7b6299abbfa05f11
beth_mccusker <b.mccusker@lancaster.ac.uk>
2025-02-18 09:21:26 -0600

    adding histograms

--- waveform/waveform_plotter.C
+++ waveform/waveform_plotter.C
@@ -54,6 +54,9 @@ std::pair<double, double> CalculateHalfWidthHeightAndAmplitudeInduction(const st
 }
 
 void waveform_plotter() {
+  gStyle->SetOptStat(0); //Removing the stats box
+  gStyle->SetPalette(kCandy);
+  TColor::InvertPalette();
   // Open the ROOT file
   TFile *file = TFile::Open("/exp/sbnd/data/users/bethanym/wire_transparency/filter_test/hists_decode_data_evb01_EventBuilder1_art1_run16740_10_20240912T082517-30bef869-9d29-42a0-aa77-091ad9c1620d_Reco1Comm-20250214T115006.root");
 
@@ -74,10 +77,15 @@ void waveform_plotter() {
 
   const char *output_dir = "/exp/sbnd/data/users/bethanym/wire_transparency/templateFitting/";
 
+  TH2D* heat_map = new TH2D("h_wire_vs_peak_time", "",  1700, 0, 1700, 3500,0, 3500);
+  TH2D* width_map = new TH2D("h_wire_vs_width", "",  1700, 0, 1700, 500,0, 10);
+  TH2D* peak_map = new TH2D("h_wire_vs_peak_amplitude", "",  1700, 0, 1700, 500,0, 300);
+  TH2D* peak_vs_width = new TH2D("h_peak_vs_width", "",  500, 0,300, 500,0, 10);
+
   // Map to store waveform data indexed by waveform number
   std::map<int, std::vector<double>> waveform_adc_map;
   std::map<int, std::vector<double>> waveform_time_map;
-  std::map<int, std::vector<double>> waveform_wire_map;
+  std::map<int, std::vector<int>> waveform_wire_map;
 
   // Loop over tree entries (events)
   Long64_t nEntries = tree->GetEntries();
@@ -102,18 +110,25 @@ void waveform_plotter() {
     int wave_num = entry.first; // The current waveform number
     const std::vector<double> &adc_vals = entry.second;
     const std::vector<double> &time_vals = waveform_time_map[wave_num];
-
+    const std::vector<int> &wire_num = waveform_wire_map[wave_num];
     // If no data, skip this waveform
     if (adc_vals.empty() || time_vals.empty()) {
       continue;
     }
 
     auto [half_width, amplitude] = CalculateHalfWidthHeightAndAmplitudeCollection(adc_vals, time_vals);
-    std::cout << "Waveform " << wave_num << " Half-Width Height: " << half_width << " Amplitude: " << amplitude << "\n";
+    //    std::cout << "Waveform " << wave_num << " Half-Width Height: " << half_width << " Amplitude: " << amplitude << "\n";
+    // std::cout << " Wire " <<wire_num[0]<< endl; 
+    // std::cout << " Time " <<time_vals[0]<< endl;
+    // std::cout << " Wave " <<wave_num<< endl;
 
+    //    auto [trough_half_width, trough_amplitude] = CalculateHalfWidthHeightAndAmplitudeInduction(adc_vals, time_vals);
+    //    std::cout << " | Trough Half-Width: " << trough_half_width << " Trough Amplitude: " << trough_amplitude << "\n";
 
-    auto [trough_half_width, trough_amplitude] = CalculateHalfWidthHeightAndAmplitudeInduction(adc_vals, time_vals);
-    std::cout << " | Trough Half-Width: " << trough_half_width << " Trough Amplitude: " << trough_amplitude << "\n";
+    heat_map->Fill(wire_num[0],time_vals[0]);
+    width_map->Fill(wire_num[0],half_width);
+    peak_map->Fill(wire_num[0],amplitude);
+    peak_vs_width->Fill(half_width,amplitude);
 
     // Create a TGraph for the current waveform
     TGraph *graph = new TGraph(adc_vals.size(), &time_vals[0], &adc_vals[0]);
@@ -132,6 +147,30 @@ void waveform_plotter() {
     delete c1;
     delete graph;
   }
+
+  TCanvas* heat = new TCanvas("heat", "2D Histogram Canvas", 800, 600);
+  heat_map->GetXaxis()->SetTitle("Wire Number");
+  heat_map->GetYaxis()->SetTitle("Time (ticks)");
+  heat_map->Draw("COLZ");
+  heat->SaveAs(Form("%sheat_map.pdf", output_dir));
+
+  TCanvas* width = new TCanvas("width", "2D Histogram Canvas", 800, 600);
+  width_map->GetXaxis()->SetTitle("Wire Number");
+  width_map->GetYaxis()->SetTitle("Width at Half Height (ticks)");
+  width_map->Draw("COLZ");
+  width->SaveAs(Form("%swidth_map.pdf", output_dir));
+
+  TCanvas* peak = new TCanvas("peak", "2D Histogram Canvas", 800, 600);
+  peak_map->GetXaxis()->SetTitle("Wire Number");
+  peak_map->GetYaxis()->SetTitle("Peak Amplitude (ADC)");
+  peak_map->Draw("COLZ");
+  peak->SaveAs(Form("%speak_map.pdf", output_dir));
+
+  TCanvas* peakwidth = new TCanvas("peakwidth", "2D Histogram Canvas", 800, 600);
+  peak_vs_width->GetXaxis()->SetTitle("Width at Half Height (ticks)");
+  peak_vs_width->GetYaxis()->SetTitle("Peak Amplitude (ADC)");
+  peak_vs_width->Draw("COLZ");
+  peakwidth->SaveAs(Form("%sheat_map.pdf", output_dir));
 
   // Clean up
   delete file;
