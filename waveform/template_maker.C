9cfc36d29dafe289460fd367bc202dcaab0d30a9
beth_mccusker <b.mccusker@lancaster.ac.uk>
2025-05-30 14:05:56 -0500

    bug fixed

--- waveform/template_maker.C
+++ waveform/template_maker.C
@@ -5,6 +5,8 @@
 #include "TH1F.h"
 #include <iostream>
 #include <TH2D.h>
+#include <tuple>
+#include "TMath.h"
 //*********************************** Functions Start ******************************************//
 TH1D* CalculateAverageHistogram(const std::vector<std::vector<TH1D*>>& histograms, int nBins) {
   // Create the final histogram to store averages
@@ -127,27 +129,34 @@ void CompareWaveformToTemplate(TH1D* individualWaveform, TH1D* averageTemplate, 
   heat_map->Fill(wire_num, hit_tim);
 
   // Create a canvas for comparison plot
-  // TCanvas *c_compare = new TCanvas(Form("c_compare_%d", wave_num), "Comparison with Template", 800, 600);
+   TCanvas *c_compare = new TCanvas(Form("c_compare_%d", wave_num), "Comparison with Template", 800, 600);
 
 
-  // Draw the individual waveform                                                                                                                                                                                    
-  //  individualWaveform->SetLineColor(kRed-6);
-  // individualWaveform->SetLineWidth(2);
-  // individualWaveform->Draw("HIST");
+  
+   individualWaveform->SetLineColor(kRed-6);
+   individualWaveform->SetLineWidth(2);
+   individualWaveform->Draw("HIST SAME");
 
-  // Draw the scaled template on the same canvas                                                                                                                                                                     
-  // Template->SetLineColor(kBlue-6);
-  // Template->SetLineWidth(2);
-  // Template->Draw("HIST SAME");
+  // Draw the scaled template on the same canvas                                               
+   TLine *line = new TLine(1, 0,41, 0);
+   line->SetLineStyle(2);    // Dashed                                                        
+   line->SetLineColor(kGray+1); // Red color                                         
+   line->SetLineWidth(2);    // Line thickness                                                
+   line->Draw("SAME");                                                                                                                    
+   Template->SetLineColor(kBlue-6);
+   Template->SetLineWidth(2);
+   Template->Draw("HIST SAME");
 
    // Add a legend to differentiate the plots
-  // TLegend *legend = new TLegend(0.7, 0.7, 0.9, 0.9);
-  // legend->AddEntry(individualWaveform, "Individual Waveform", "l");
-  // legend->AddEntry(Template, "Scaled Template", "l");
-  // legend->Draw();
+   TLegend *legend = new TLegend(0.65, 0.7, 0.9, 0.9);
+   legend->AddEntry(individualWaveform, "Individual Waveform", "l");
+   legend->AddEntry(Template, "Scaled Template", "l");
+   legend->SetTextSize(0.03);
+   legend->Draw();
+
 
   // Save the comparison plot
-  // c_compare->SaveAs(Form("%scomparison_waveform_%d.pdf", output_dir, wave_num));
+   c_compare->SaveAs(Form("%scomparison_waveform_%d.pdf", output_dir, wave_num));
 
   // Clean up
   // delete c_compare;
@@ -159,7 +168,7 @@ void CompareWaveformToTemplate(TH1D* individualWaveform, TH1D* averageTemplate, 
 std::pair<double, double> findIntersection(double m1, double c1, double m2, double c2) {
   // Check if the lines are parallel
   if (m1 == m2) {
-    std::cerr << "The lines are parallel and do not intersect." << std::endl;
+    // std::cerr << "The lines are parallel and do not intersect." << std::endl;
     return {std::numeric_limits<double>::quiet_NaN(), std::numeric_limits<double>::quiet_NaN()};
   }
 
@@ -179,6 +188,33 @@ double vector_to_double(const std::vector<double>& vec) {
   return vec[0]; 
 }
 
+
+std::vector<std::tuple<double, int>> GetBinValuesAndCounts(TH2D* h_values, TH2D* h_counts) {
+  std::vector<std::tuple<double, int>> binData;
+
+  // Ensure histograms have the same binning
+  if (!h_values || !h_counts || 
+      h_values->GetNbinsX() != h_counts->GetNbinsX() || 
+      h_values->GetNbinsY() != h_counts->GetNbinsY()) {
+    std::cerr << "Error: Histograms are null or have different binning!" << std::endl;
+    return binData;
+  }
+
+  // Loop over all bins (excluding underflow/overflow)
+  for (int xBin = 1; xBin <= h_values->GetNbinsX(); xBin++) {
+    for (int yBin = 1; yBin <= h_values->GetNbinsY(); yBin++) {
+      double binValue = h_values->GetBinContent(xBin, yBin);
+      int binCount = static_cast<int>(h_counts->GetBinContent(xBin, yBin));
+            
+      // Store (value, count) pair in the vector
+      binData.push_back(std::make_tuple(binValue, binCount));
+    }
+  }
+
+  return binData;
+}
+
+
 //*********************************************Functions End ***************************************************//
 
 
@@ -187,7 +223,7 @@ void template_maker() {
   gStyle->SetOptStat(0); //Removing the stats box
    gStyle->SetPalette(kCandy);
    TColor::InvertPalette(); 
-  TFile *file = TFile::Open("/exp/sbnd/data/users/bethanym/wire_transparency/grid_test/merged.root");
+  TFile *file = TFile::Open("/exp/sbnd/data/users/bethanym/wire_transparency/transparency_samples/TPC0Plane2_oldnom.root");
   TTree *tree = (TTree*)file->Get("hitdumper/hitdumpertree");
 
   // Variables to store the data
@@ -200,6 +236,7 @@ void template_maker() {
   std::vector<double> *hit_time = nullptr;
   std::vector<double> *crt_gradient=nullptr;
   std::vector<double> *crt_intercept=nullptr;
+  std::vector<double> *theta_yz_CRT=nullptr;
  
   // Set branch addresses to connect the variables to the tree
   tree->SetBranchAddress("nhits", &nhits); 
@@ -211,6 +248,7 @@ void template_maker() {
   tree->SetBranchAddress("hit_time", &hit_time);
   tree->SetBranchAddress("crt_gradient", &crt_gradient);
   tree->SetBranchAddress("crt_intercept", &crt_intercept);
+  tree->SetBranchAddress("theta_yz_CRT", &theta_yz_CRT);
 
 
 
@@ -233,18 +271,22 @@ void template_maker() {
   std::vector<std::map<int, int>> waveform_channel_map;
   std::vector<std::map<int, double>> waveform_peak_time_map;
 
-  TH2D* h_sum = new TH2D("sum entriws", "2D Histogram;Y coordinate;Z coordinate;Scaling Probability",175, 0., 500.,100, -200., 200.);
-  TH2D* h_count = new TH2D("count entries", "Histogram;Y coordinate;Z coordinate;Scaling Probability",175, 0., 500.,100, -200., 200.); 
-
+  TH2D* h_sum = new TH2D("sum entriws", "2D Histogram;Y coordinate;Z coordinate;Average Scaling",20, 0., 500.,20, -200., 200.);
+  TH2D* h_count = new TH2D("count entries", "Histogram;Y coordinate;Z coordinate;Average Scaling",20, 0., 500.,20, -200., 200.); 
+  TH1D* h_waveform_count = new TH1D("h_waveform_count", "Waveform Count per Scaling Factor Bin;Scaling Factor;Count", 50, 0, 2);
+  TH2D* h_correlation = new TH2D("correlation", "",50, 0.5, 1.2,50, 0, 1500);
   // Loop over tree entries (events)
   Long64_t nEntries = tree->GetEntries();
   for (Long64_t i = 0; i < nEntries; i++) {
+
     tree->GetEntry(i); // Load the ith entry
     double crt_grad=0;
     double crt_int=0;
-    if(crt_gradient->size()==1){
+    double crt_yz=0;
+    if(crt_gradient->size()==1 && crt_intercept->size()==1 && theta_yz_CRT->size()==1){
       crt_grad=vector_to_double(*crt_gradient);
       crt_int=vector_to_double(*crt_intercept);
+      crt_yz=vector_to_double(*theta_yz_CRT);
     }
    
     // Map to store waveform data indexed by waveform number                                       
@@ -276,7 +318,7 @@ void template_maker() {
       const std::vector<double>& adc_vals = entry.second;
      
       // Create a histogram for the waveform
-      TH1D* hist = new TH1D(Form("waveform_%d_entry_%lld", wave_num, i), Form("Waveform %d;Time (ticks);ADC counts", wave_num), nBins, 1, 41);
+      TH1D* hist = new TH1D(Form("waveform_%d_entry_%lld", wave_num, i), Form("Waveform %d;Time (ticks);ADC (pedestal-subtracted)", wave_num), nBins, 1, 41);
 
       // Fill the histogram with ADC values at corresponding time bins
       for (size_t k = 0; k < adc_vals.size(); k++) {
@@ -311,6 +353,7 @@ void template_maker() {
   averageHistogram->SetLineWidth(3);
   averageHistogram->Draw();
   c_avg->SaveAs(Form("%stemplate_waveform.pdf", output_dir));
+  c_avg->SaveAs(Form("%stemplate_waveform.C", output_dir));
 
   delete c_avg;
 
@@ -318,32 +361,41 @@ void template_maker() {
   // Compare individual waveforms to the average template and plot                                                                                                                                          
 for (int i = 0; i < nEntries; i++) {
   tree->GetEntry(i);
+  std::cout << "Starting entry " << i << std::endl;
   if (nhits == 0) {
     std::cout << "no waveforms or hits for entry " << i << std::endl;
     continue;
   } 
+ 
+  //  TGraph* scalingGraph = new TGraph();
+  // TH2D* heat_map = new TH2D("h_wire_vs_peak_time", "Wire Number vs Peak Time",  1700, 0, 1700, 3500,0, 3500);
 
-  TGraph* scalingGraph = new TGraph();
-  TH2D* heat_map = new TH2D("h_wire_vs_peak_time", "Wire Number vs Peak Time",  1700, 0, 1700, 3500,0, 3500);
-  double crt_grad=vector_to_double(*crt_gradient);
-  double crt_int=vector_to_double(*crt_intercept);
+  double crt_grad=0;
+  double crt_int=0;
+  double crt_yz=0;
+  if(crt_gradient->size()==1 && crt_intercept->size()==1 && theta_yz_CRT->size()==1){
+    crt_grad=vector_to_double(*crt_gradient);
+    crt_int=vector_to_double(*crt_intercept);
+    crt_yz=vector_to_double(*theta_yz_CRT);
+  }
+ 
           
   int wave_num=1;
   double sumScalingFactors = 0.0;
   int countScalingFactors = 0;
-  for (auto hist : histograms.at(i)) {
-    if (waveform_wire_map.at(i).find(wave_num) == waveform_wire_map.at(i).end()) {
-      std::cerr << "Waveform number " << wave_num << " not found in map for entry " << i << std::endl;
-      continue;
-    }
+ 
+ for (auto hist : histograms.at(i)) {
+ 
     double hit_tim=waveform_peak_time_map.at(i)[wave_num];
     int wire_num = waveform_wire_map.at(i)[wave_num];
     int channel_num = waveform_channel_map.at(i)[wave_num];
     // std::cout<<"chan number" << channel_num<<std::endl;
-    CompareWaveformToTemplate(hist, averageHistogram, output_dir, wave_num,wire_num,scalingGraph,hit_tim, heat_map);
+    //    CompareWaveformToTemplate(hist, averageHistogram, output_dir, wave_num,wire_num,scalingGraph,hit_tim, heat_map);
     double scalingFactor = CalculateScalingFactor(hist, averageHistogram);
     sumScalingFactors += scalingFactor;
     countScalingFactors++;
+    int bin = h_waveform_count->FindBin(scalingFactor);
+    h_waveform_count->SetBinContent(bin, h_waveform_count->GetBinContent(bin) + 1);
     int nentries=tree2->GetEntries();
     for (int entry = 0; entry < nentries; ++entry) {
       tree2->GetEntry(entry);
@@ -356,34 +408,68 @@ for (int i = 0; i < nEntries; i++) {
 	  double y_position = yz_position.first;
 	  double z_position= yz_position.second;
 	  // std::cout<< "y=  "<< y_position<<std::endl;
-	  // std::cout<< "z=  "<< z_position<<std::endl;
-	  h_sum->Fill(z_position, y_position, scalingFactor);
+	   //	   std::cout<< "z=  "<< z_position<<std::endl;
+
+	  h_sum->Fill(z_position, y_position, scalingFactor*cos(crt_yz*M_PI/180.));
 	  h_count->Fill(z_position,y_position,1);
+
 	}
       }
     }
     wave_num++;
   }
-    
+
   if (countScalingFactors > 0) {
     double averageScalingFactor = sumScalingFactors / countScalingFactors;
     eventScalingFactors.push_back(averageScalingFactor);
     std::cout << "Average scaling factor for entry " << i << ": " << averageScalingFactor << std::endl;
   }
+  // delete scalingGraph;
+  // delete heat_map; 
+
+ }
   TH2D* h_avg = (TH2D*)h_sum->Clone("h_avg");
   h_avg->SetTitle("Averaged Values");
   h_avg->Divide(h_count);
   TCanvas* average = new TCanvas("averagw", "2D Histogram Canvas", 800, 600);
-  h_avg->GetXaxis()->SetTitle("Z");
-  h_avg->GetYaxis()->SetTitle("Y");
+  h_avg->GetXaxis()->SetTitle("Detector Coordinate Z (cm)");
+  h_avg->GetYaxis()->SetTitle("Detector Coordinate Y (cm)");
+  h_avg->GetZaxis()->SetRangeUser(0.8, 1.2);
   h_avg->Draw("COLZ");
+  gPad->SetRightMargin(0.15);
+  h_avg->GetYaxis()->SetTitleOffset(1.1);
+  h_avg->GetZaxis()->SetTitleOffset(1.2);
+  gPad->Update();
   average->SaveAs(Form("%saverage_scaling_across_dectector.pdf", output_dir));
- 
+  average->SaveAs(Form("%saverage_scaling_across_dectector.C", output_dir)); 
+
  TCanvas* sum = new TCanvas("sum", "2D Histogram Canvas", 800, 600);
   h_sum->GetXaxis()->SetTitle("Z");
   h_sum->GetYaxis()->SetTitle("Y");
+  h_sum->GetZaxis()->SetRangeUser(0.8, 1.2);
   h_sum->Draw("COLZ");
   sum->SaveAs(Form("%ssum_scaling_across_dectector.pdf", output_dir));
+
+  auto results = GetBinValuesAndCounts(h_avg, h_sum);
+
+  for (const auto& [value, count] : results) {
+    std::cout << "Value: " << value << ", Count: " << count << std::endl;
+    h_correlation->Fill(value,count);
+  }
+
+  TCanvas* correlation = new TCanvas("correlation", "2D Histogram Canvas", 800, 600);
+  h_correlation->GetXaxis()->SetTitle("Average Scaling");
+  h_correlation->GetYaxis()->SetTitle("Number of Waveforms");
+  h_correlation->Draw("COLZ");
+  correlation->SaveAs(Form("%scorrelation.pdf", output_dir));
+
+
+
+  TCanvas* c_waveform_count = new TCanvas("c_waveform_count", "Waveform Count per Scaling Factor", 800, 600);
+  h_waveform_count->SetLineColor(kRed-6);
+  h_waveform_count->SetLineWidth(2);
+  h_waveform_count->Draw();
+  c_waveform_count->SaveAs(Form("%swaveform_count.pdf", output_dir));
 
   // Plot and save the scaling factor vs wire number
   // TCanvas *c_scaling = new TCanvas("c_scaling", "Scaling Factor vs Wire Number", 800, 600);
@@ -403,13 +489,13 @@ for (int i = 0; i < nEntries; i++) {
   //  c_wire_vs_time->SaveAs(Form("%sheat_map_entry_%d.pdf", output_dir, i));
 
   // Clean up
-  delete scalingGraph;
-  delete heat_map;
-  delete h_sum;
-  delete h_avg;
+  //  delete scalingGraph;
+  // delete heat_map;
+  // delete h_sum;
+  // delete h_avg;
   delete average;
   delete sum;
- }
+ 
 }
 
 
